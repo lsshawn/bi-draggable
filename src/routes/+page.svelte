d1511b7f0870973ab9008b469ec3132e694031c6
sshawn <github.mlxup@simplelogin.com>
2024-04-22 14:00:09 +0800

    UI demo

---
+++ src/routes/+page.svelte
@@ -0,0 +1,74 @@
+<script lang="ts">
+	import BarChart from '$lib/components/BarChart.svelte';
+	import LineChart from '$lib/components/LineChart.svelte';
+	import Grid, { GridItem } from 'svelte-grid-extended';
+
+	let components = [
+		{
+			name: 'LineChart',
+			component: LineChart,
+			x: 0,
+			y: 0,
+			w: 4,
+			h: 5
+		},
+		{
+			name: 'BarChart',
+			component: BarChart,
+			x: 4,
+			y: 0,
+			w: 4,
+			h: 5
+		}
+	];
+	const itemSize = { width: 100, height: 40 };
+
+	function addLineChart() {
+		components = [
+			...components,
+			{
+				name: 'LineChart',
+				component: LineChart,
+				x: 0,
+				y: 0,
+				w: 4,
+				h: 5
+			}
+		];
+	}
+
+	function addBarChart() {
+		components = [
+			...components,
+			{
+				name: 'BarChart',
+				component: BarChart,
+				x: 0,
+				y: 0,
+				w: 4,
+				h: 5
+			}
+		];
+	}
+</script>
+
+<div class="min-h-screen p-8">
+	<div class="flex gap-2">
+		<button class="btn" on:click={addLineChart}>Add Line Chart</button>
+		<button class="btn" on:click={addBarChart}>Add Bar Chart</button>
+	</div>
+
+	<Grid {itemSize} cols={10} rows={10} class="mt-16 p-0" collision="compress">
+		{#each components as component}
+			<GridItem
+				bind:x={component.x}
+				bind:y={component.y}
+				bind:w={component.w}
+				bind:h={component.h}
+				class="item"
+			>
+				<svelte:component this={component.component} />
+			</GridItem>
+		{/each}
+	</Grid>
+</div>
---
+++ tailwind.config.cjs
@@ -0,0 +1,17 @@
+const daisyui = require('daisyui');
+const typography = require('@tailwindcss/typography');
+const forms = require('@tailwindcss/forms');
+
+/** @type {import('tailwindcss').Config}*/
+const config = {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+
+	theme: {
+		extend: {}
+	},
+
+	plugins: [forms, typography, daisyui],
+  daisyui: { logs: false}
+};
+
+module.exports = config;
